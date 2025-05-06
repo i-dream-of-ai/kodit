47a3357f222627c0e89d90509bb83c245e6208ce
Phil Winder <phil@winder.ai>
2025-05-06 12:45:56 +0100

    feat(packaging): pypi and docker (#10)

--- src/kodit/cli.py
+++ src/kodit/cli.py
@@ -51,5 +51,16 @@ def serve(
     )
 
 
+@cli.command()
+def version() -> None:
+    """Show the version of kodit."""
+    try:
+        from kodit import _version
+    except ImportError:
+        print("unknown, try running `uv build`, which is what happens in ci")  # noqa: T201
+    else:
+        print(_version.version)  # noqa: T201
+
+
 if __name__ == "__main__":
     cli()
